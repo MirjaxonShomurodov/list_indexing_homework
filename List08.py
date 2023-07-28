4d757bb8c4ceae939b2bd077a2344ef281d8f1e8
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-28 16:23:13 +0500

    8 misol

--- List08.py
+++ List08.py
@@ -6,4 +6,16 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+      
+    i = 0
+    while i<len(list1):
+        if list1[i] != 1: # list1[i] == 1
+            list1[i] = bool(list1[i])
+        i += 1
+        if list1[i] != 0: # list1[i] == 1
+            list1[i] = bool(list1[i])
+        i += 1
+    return list1
+        
+list1=[0,1,0,1,0,1]    
+print(main(list1))
